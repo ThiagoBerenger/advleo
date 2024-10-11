525e7ec26719b66f4ca81096869702620006287e
Thiago <thberenger27@gmail.com>
2024-10-11 14:45:06 -0300

    atualizando GA4

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
-import React, { useEffect } from 'react'
-import ReactGA from 'react-ga';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga4'; // use react-ga4 se for GA4
 
-import { Outlet, useLocation } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom';
 
-import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer'
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
-
-ReactGA.initialize('G-NY7YPXF0DW');
+// Inicializa o Google Analytics apenas no ambiente de produção
+if (process.env.NODE_ENV === 'production') {
+  ReactGA.initialize('G-NY7YPXF0DW');
+}
 
 function App() {
-
-  const location = useLocation(); 
+  const location = useLocation();
 
   useEffect(() => {
-    // Enviar uma página vista sempre que a rota mudar
-    ReactGA.pageview(location.pathname + location.search);
+    // Verifica se o GA está configurado (no ambiente de produção)
+    if (process.env.NODE_ENV === 'production') {
+      // Envia uma visualização de página sempre que a rota mudar
+      ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search });
+    }
   }, [location]);
 
   return (
-      <div className="App">
-        <Header/>
-          <Outlet/>
-        <Footer/>
-      </div>
-  )
+    <div className="App">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
