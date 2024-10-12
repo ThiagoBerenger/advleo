ac1d3a8887f7ac97932cb82a49282631a71a0745
Thiago <thberenger27@gmail.com>
2024-10-12 20:50:38 -0300

    atualizando sitemap

--- sitemap-builder.js
+++ sitemap-builder.js
@@ -1,30 +1,32 @@
 import { SitemapStream } from 'sitemap';
-import { createWriteStream } from 'fs';
+   import { createWriteStream } from 'fs';
 
-const links = [
-  { url: '/', changefreq: 'daily', priority: 1.0 },
-  { url: '/about', changefreq: 'monthly', priority: 0.8 },
-  { url: '/contact', changefreq: 'monthly', priority: 0.8 },
-  // Adicione todas as suas rotas aqui
-];
+   const links = [
+     { url: '/', changefreq: 'daily', priority: 1.0 },
+     { url: '/extravio-de-bagagem', changefreq: 'monthly', priority: 0.8 },
+     { url: '/atraso-de-voo', changefreq: 'monthly', priority: 0.8 },
+     { url: '/cancelamento-de-voo', changefreq: 'monthly', priority: 0.8 },
+     { url: '/overbooking', changefreq: 'monthly', priority: 0.8 },
+     // Adicione outras rotas que você deseja rastrear
+   ];
 
-async function generateSitemap() {
-  const sitemap = new SitemapStream({ hostname: 'https://advleo.com.br' });
-  const writeStream = createWriteStream('./public/sitemap.xml');
+   async function generateSitemap() {
+     const sitemap = new SitemapStream({ hostname: 'https://advleo.com.br' });
+     const writeStream = createWriteStream('./public/sitemap.xml');
 
-  // Usar uma promise diretamente para aguardar o final do stream
-  await new Promise((resolve, reject) => {
-    sitemap.pipe(writeStream)
-      .on('finish', resolve)  // Quando terminar o stream, resolver a promise
-      .on('error', reject);   // Se ocorrer um erro, rejeitar a promise
+     // Usar uma promise diretamente para aguardar o final do stream
+     await new Promise((resolve, reject) => {
+       sitemap.pipe(writeStream)
+         .on('finish', resolve)
+         .on('error', reject);
 
-    // Escrever os links no stream
-    links.forEach(link => sitemap.write(link));
+       // Escrever os links no stream
+       links.forEach(link => sitemap.write(link));
 
-    sitemap.end();
-  });
+       sitemap.end();
+     });
 
-  console.log('Sitemap gerado com sucesso!');
-}
+     console.log('Sitemap gerado com sucesso!');
+   }
 
-generateSitemap();
+   generateSitemap();
